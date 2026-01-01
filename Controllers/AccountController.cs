df4e801c5becab0bc051af397d5da81462df392a
VictoriaGleam <Radbergvictoria@gmail.com>
2026-01-01 20:48:11 +0100

    Add GET actions for Login and Register

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,12 +4,28 @@ namespace TheFitnessApp_1.Controllers
 {
     public class AccountController : Controller
     {
+        // Visar login-sidan
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // Hanterar login-formuläret
         [HttpPost]
         public IActionResult Login(string email, string password)
         {
             return RedirectToAction("Welcome", "Home");
         }
 
+        // Visar register-sidan
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // Hanterar register-formuläret
         [HttpPost]
         public IActionResult Register(string firstName, string lastName, string email, string password)
         {
